6068a89a995c023fd3f002b66cf177d23089605e
altunog <altunog@gmail.com>
2022-10-25 15:59:44 +0300

    Create project

---
+++ Pazaryeri/Pazaryeri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
