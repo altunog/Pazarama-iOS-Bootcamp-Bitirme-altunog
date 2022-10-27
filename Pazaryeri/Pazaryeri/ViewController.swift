11e258f5e9285af0c2be1e83baa44ef2f371308a
altunog <altunog@gmail.com>
2022-10-27 13:26:10 +0300

    remove Storyboard, setup AppDelegate window

--- Pazaryeri/Pazaryeri/ViewController.swift
+++ Pazaryeri/Pazaryeri/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		view.backgroundColor = .yellow
 	}
 
 
