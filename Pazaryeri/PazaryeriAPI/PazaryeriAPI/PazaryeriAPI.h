a6762d7c93596a9b65392347335482e5c5ab7e34
altunog <altunog@gmail.com>
2022-11-01 17:19:03 +0300

    create workspace file, create and add PazaryeriAPI framework to Pazaryeri project

---
+++ Pazaryeri/PazaryeriAPI/PazaryeriAPI/PazaryeriAPI.h
@@ -0,0 +1,18 @@
+//
+//  PazaryeriAPI.h
+//  PazaryeriAPI
+//
+//  Created by Oğuz Kaan Altun on 1.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PazaryeriAPI.
+FOUNDATION_EXPORT double PazaryeriAPIVersionNumber;
+
+//! Project version string for PazaryeriAPI.
+FOUNDATION_EXPORT const unsigned char PazaryeriAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PazaryeriAPI/PublicHeader.h>
+
+
