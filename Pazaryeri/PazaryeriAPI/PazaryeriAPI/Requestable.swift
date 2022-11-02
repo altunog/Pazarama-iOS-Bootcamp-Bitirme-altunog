0c9131ee749f772dd9c3226da8b8dfcbc671bdd7
altunog <altunog@gmail.com>
2022-11-02 12:25:59 +0300

    add decode function to Requestable protocol

--- Pazaryeri/PazaryeriAPI/PazaryeriAPI/Requestable.swift
+++ Pazaryeri/PazaryeriAPI/PazaryeriAPI/Requestable.swift
@@ -8,12 +8,15 @@
 import Foundation
 
 protocol Requestable {
+	associatedtype Response
 	
 	var baseURL: String { get }
 	var path: String { get }
 	var method: HTTPMethod { get }
 	var headers: [String: String] { get }
 	var queryItems: [String: String] { get }
+	
+	func decode(_ data: Data) throws -> Response
 }
 
 
@@ -25,5 +28,13 @@ extension Requestable {
 	
 	var queryItems: [String: String] {
 		[:]
+	}
+}
+
+extension Requestable where Response: Decodable {
+	
+	func decode(_ data: Data) throws -> Response {
+		let decoder = JSONDecoder()
+		return try decoder.decode(Response.self, from: data)
 	}
 }
