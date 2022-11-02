5bf682b849b9ad405231f82d0ca9134b9170860c
altunog <altunog@gmail.com>
2022-11-02 11:21:49 +0300

    create and configure HTTPMethod file

---
+++ Pazaryeri/PazaryeriAPI/PazaryeriAPI/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  PazaryeriAPI
+//
+//  Created by Oğuz Kaan Altun on 2.11.2022.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+	case get 	= "GET"
+	case post 	= "POST"
+	case put 	= "PUT"
+	case patch 	= "PATCH"
+	case delete	= "DELETE"
+}
--- Pazaryeri/PazaryeriAPI/PazaryeriAPI/PYError.swift
+++ Pazaryeri/PazaryeriAPI/PazaryeriAPI/PYError.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum PYError: String, Error {
-	case invalidEnpoint = "Invalid endpoint. Please try again, and contact our support team if the problem persists."
-	case invalidResponse = "Invalid response from the service. Please try again."
-	case invalidData = "The data received from the server was invalid. Please try again."
-	case unableToComplete = "Unable to complete your request. Please check your internet connection"
+	case invalidEnpoint 	= "Invalid endpoint. Please try again, and contact our support team if the problem persists."
+	case invalidResponse	= "Invalid response from the service. Please try again."
+	case invalidData 		= "The data received from the server was invalid. Please try again."
+	case unableToComplete 	= "Unable to complete your request. Please check your internet connection"
 }
