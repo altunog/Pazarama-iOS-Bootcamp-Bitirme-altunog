52abe24a9218b4aca2a8da80d9c89dd03941f280
altunog <altunog@gmail.com>
2022-11-03 00:04:32 +0300

    configure and run the api test

--- Pazaryeri/PazaryeriAPI/PazaryeriAPITests/PazaryeriAPITests.swift
+++ Pazaryeri/PazaryeriAPI/PazaryeriAPITests/PazaryeriAPITests.swift
@@ -24,6 +24,21 @@ final class PazaryeriAPITests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+		let bundle = Bundle(for: PazaryeriAPITests.self)
+		let url = bundle.url(forResource: "product", withExtension: "json")!
+		let data = try Data(contentsOf: url)
+		let products = try JSONDecoder().decode([Product].self, from: data)
+		print(products)
+		
+		XCTAssertEqual(products.count, 1, "Unexpected product number.")
+		XCTAssertEqual(products.first?.id, 1, "Unexpected id.")
+		XCTAssertEqual(products.first?.title, "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", "Unexpected title.")
+		XCTAssertEqual(products.first?.price, 109.95, "Unexpected price.")
+		XCTAssertEqual(products.first?.description, "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday", "Unexpected description.")
+		XCTAssertEqual(products.first?.category, "men's clothing", "Unexpected category.")
+		XCTAssertEqual(products.first?.image, "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", "Unexpected image.")
+		XCTAssertEqual(products.first?.rating?.rate, 3.9, "Unexpected rate item of rating.")
+		XCTAssertEqual(products.first?.rating?.count, 120, "Unexpected count item of rating.")
     }
 
     func testPerformanceExample() throws {
