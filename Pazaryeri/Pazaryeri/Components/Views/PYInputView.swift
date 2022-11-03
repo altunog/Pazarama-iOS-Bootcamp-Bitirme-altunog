49e8ab38e29c0d42ead92238c511d5cba73f3de8
altunog <altunog@gmail.com>
2022-11-04 02:04:30 +0300

    create PYTextField component, configure PYInputView

--- Pazaryeri/Pazaryeri/Components/Views/PYInputView.swift
+++ Pazaryeri/Pazaryeri/Components/Views/PYInputView.swift
@@ -9,6 +9,39 @@ import UIKit
 
 class PYInputView: UIView {
 
+	// MARK: Properties Getters and Setters
+	var title: String? {
+		didSet {
+			titleLabel.text = title
+		}
+	}
+	
+	var placeholder: String? {
+		didSet {
+			inputField.placeholder = placeholder
+		}
+	}
+	
+	var isSecureText: Bool = false {
+		didSet {
+			inputField.isSecureTextEntry = isSecureText
+		}
+	}
+	
+	// MARK: UI Elements
+	private lazy var titleLabel: PYBodyLabel = {
+		let label = PYBodyLabel(textAlignment: .left, fontSize: 11)
+		label.textColor = .gray
+		return label
+	}()
+	
+	private lazy var inputField: PYTextField = {
+		let textField = PYTextField(placeholder: "", isSecureTextEntry: false)
+		
+		return textField
+	}()
+	
+	// MARK: Init
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 	}
@@ -16,6 +49,8 @@ class PYInputView: UIView {
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+	
+	// MARK: Methods
 	
 	
 
---
+++ Pazaryeri/Pazaryeri/Components/Views/PYTextField.swift
@@ -0,0 +1,48 @@
+//
+//  PYTextField.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 4.11.2022.
+//
+
+import UIKit
+
+class PYTextField: UITextField {
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		configure()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	convenience init(placeholder: String, isSecureTextEntry: Bool) {
+		self.init(frame: .zero)
+		self.placeholder 		= placeholder
+		self.isSecureTextEntry 	= isSecureTextEntry
+		configure()
+	}
+	
+	private func configure() {
+		translatesAutoresizingMaskIntoConstraints = false
+		
+		layer.cornerRadius 			= 3
+		layer.borderWidth 			= 1
+		layer.borderColor 			= UIColor.gray.cgColor
+		
+		textColor 					= .black
+		tintColor 					= .black
+		textAlignment 				= .left
+		font 						= .preferredFont(forTextStyle: .title3)
+		adjustsFontSizeToFitWidth	= true
+		minimumFontSize 			= 16
+		
+		backgroundColor 			= .lightGray
+		autocorrectionType 			= .no
+		returnKeyType 				= .next
+		clearButtonMode 			= .whileEditing
+	}
+
+}
