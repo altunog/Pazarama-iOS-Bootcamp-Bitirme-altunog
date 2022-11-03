08564137a916a879d5c01ce2223d24b6dbdb5a22
altunog <altunog@gmail.com>
2022-11-04 01:29:59 +0300

    create PYInputView for user inputs

---
+++ Pazaryeri/Pazaryeri/Components/Views/PYInputView.swift
@@ -0,0 +1,22 @@
+//
+//  PYInputView.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 4.11.2022.
+//
+
+import UIKit
+
+class PYInputView: UIView {
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	
+
+}
