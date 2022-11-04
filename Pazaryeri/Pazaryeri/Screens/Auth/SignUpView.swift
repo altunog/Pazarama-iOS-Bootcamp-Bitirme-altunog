51b7c3c63979fbec70737329f43119cc351ea37f
altunog <altunog@gmail.com>
2022-11-04 17:14:14 +0300

    create SignUp view and controller files

---
+++ Pazaryeri/Pazaryeri/Screens/Auth/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 4.11.2022.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Pazaryeri/Pazaryeri/Screens/Auth/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 4.11.2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
