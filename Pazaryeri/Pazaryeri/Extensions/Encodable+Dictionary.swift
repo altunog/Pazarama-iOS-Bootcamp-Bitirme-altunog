a350e97c61a6536863c42aeed02496248346032a
altunog <altunog@gmail.com>
2022-11-05 01:06:36 +0300

    add extension to Encodable for parsing User objects to json

---
+++ Pazaryeri/Pazaryeri/Extensions/Encodable+Dictionary.swift
@@ -0,0 +1,16 @@
+//
+//  Encodable+Dictionary.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 5.11.2022.
+//
+
+import Foundation
+
+extension Encodable {
+
+	var dictionary: [String: Any] {
+			let data = (try? JSONEncoder().encode(self)) ?? Data()
+			return (try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]) ?? [:]
+		}
+}
