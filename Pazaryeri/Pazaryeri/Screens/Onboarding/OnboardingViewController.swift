734a2dcd900f5d1e57e659803d5079a63c5c3196
altunog <altunog@gmail.com>
2022-11-06 15:37:52 +0300

    create Onboarding screen files and folder

---
+++ Pazaryeri/Pazaryeri/Screens/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingViewController.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 6.11.2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
