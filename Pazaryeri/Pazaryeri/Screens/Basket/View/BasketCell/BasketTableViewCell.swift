8b10760493eb19b7a8d3a373fbcde72ab0b7e690
altunog <altunog@gmail.com>
2022-11-07 01:23:36 +0300

    configure basket cell

--- Pazaryeri/Pazaryeri/Screens/Basket/View/BasketCell/BasketTableViewCell.swift
+++ Pazaryeri/Pazaryeri/Screens/Basket/View/BasketCell/BasketTableViewCell.swift
@@ -7,9 +7,23 @@
 
 import UIKit
 
+protocol BasketTableViewCellInterface: AnyObject {
+	func stepperValueDidChange(_ stepper: UIStepper)
+}
+
+
 class BasketTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
+	static let reuseID = "BasketCell"
+	
+	@IBOutlet weak var productImageView: UIImageView!
+	@IBOutlet weak var productTitle: UILabel!
+	@IBOutlet weak var priceLabel: UILabel!
+	@IBOutlet weak var quantityLabel: UILabel!
+	@IBOutlet weak var stepper: UIStepper!
+	
+	
+	override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
@@ -20,4 +34,9 @@ class BasketTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+	
+	@IBAction func stepperValueChanged(_ stepper: UIStepper) {
+		
+	}
+	
 }
