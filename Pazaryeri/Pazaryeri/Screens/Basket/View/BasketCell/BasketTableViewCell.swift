5adee5c19648911b9a00dae3898712eda885a37c
altunog <altunog@gmail.com>
2022-11-07 00:42:14 +0300

    add tableview to xib, create basketCell file

---
+++ Pazaryeri/Pazaryeri/Screens/Basket/View/BasketCell/BasketTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BasketTableViewCell.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 7.11.2022.
+//
+
+import UIKit
+
+class BasketTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
