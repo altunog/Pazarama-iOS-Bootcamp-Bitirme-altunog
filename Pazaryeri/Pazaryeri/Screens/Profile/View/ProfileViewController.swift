87277ad9fff240bc4e00ab3ce91617ad3ed2ee57
altunog <altunog@gmail.com>
2022-11-07 06:26:04 +0300

    create and configure profile xib file, create profile viewmodel

---
+++ Pazaryeri/Pazaryeri/Screens/Profile/View/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 7.11.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+	@IBOutlet weak var usernameLabel: UILabel!
+	@IBOutlet weak var emailLabel: UILabel!
+	@IBOutlet weak var logoutButton: UIButton!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+	
+
+	@IBAction func logoutButtonTapped(_ sender: UIButton) {
+		
+	}
+}
---
+++ Pazaryeri/Pazaryeri/Screens/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewModel.swift
+//  Pazaryeri
+//
+//  Created by Oğuz Kaan Altun on 7.11.2022.
+//
+
+import Foundation
+
+
+final class ProfileViewModel {
+	
+	
+}
