751db1c7350de2c32341e70c66cac207dfc35058
altunog <altunog@gmail.com>
2022-11-07 06:42:49 +0300

    configure profile viewmodel and logout operation

--- Pazaryeri/Pazaryeri/Screens/Profile/View/ProfileViewController.swift
+++ Pazaryeri/Pazaryeri/Screens/Profile/View/ProfileViewController.swift
@@ -9,18 +9,42 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+	let viewModel = ProfileViewModel()
+	
 	@IBOutlet weak var usernameLabel: UILabel!
 	@IBOutlet weak var emailLabel: UILabel!
 	@IBOutlet weak var logoutButton: UIButton!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		
+		configureViewController()
+		viewModel.fetchUser()
+		configureLabels()
     }
 	
+	private func configureViewController() {
+		viewModel.delegate = self
+	}
+	
+	private func configureLabels() {
+		usernameLabel.text 	= "Username: \(viewModel.username ?? "not available")"
+		emailLabel.text		= "Email: \(viewModel.email ?? "not available")"
+	}
+	
 
-	@IBAction func logoutButtonTapped(_ sender: UIButton) {
-		
+	@IBAction func logoutButtonTapped(_ button: UIButton) {
+		// TODO: show alert
+		viewModel.signOut()
+	}
+}
+
+extension ProfileViewController: ProfileViewModelDelegate {
+	func errorDidOccur(_ error: Error) {
+		print(error.localizedDescription)
+	}
+	
+	func didSignOutSuccessful() {
+
 	}
 }
--- Pazaryeri/Pazaryeri/Screens/Profile/ViewModel/ProfileViewModel.swift
+++ Pazaryeri/Pazaryeri/Screens/Profile/ViewModel/ProfileViewModel.swift
@@ -6,9 +6,35 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
+protocol ProfileViewModelDelegate: AnyObject {
+	func errorDidOccur(_ error: Error)
+	func didSignOutSuccessful()
+}
 
 final class ProfileViewModel {
 	
+	weak var delegate: ProfileViewModelDelegate?
+	let currentUser = Auth.auth().currentUser
 	
+	private(set) var username: String?
+	private(set) var email: String?
+	
+	func fetchUser() {
+		if let currentUser {
+			username 	= currentUser.displayName
+			email		= currentUser.email
+		}
+	}
+	
+	func signOut() {
+		do {
+			try Auth.auth().signOut()
+			delegate?.didSignOutSuccessful()
+		} catch {
+			delegate?.errorDidOccur(error)
+		}
+	}
 }
